48e34507a639cb52f4ade4bdff7921c3445dc036
Bastien Dejean <nihilhill@gmail.com>
2013-07-16 11:06:11 +0200

    Handle failures

--- ledcns.c
+++ ledcns.c
@@ -7,9 +7,21 @@ int main(int argc, char *argv[])
 {
     int fd;
     unsigned char state;
-    if (argc < 2 || sscanf(argv[1], "%hhu", &state) != 1 || (fd = open("/dev/console", O_WRONLY)) == -1)
+    if (argc < 2 || sscanf(argv[1], "%hhu", &state) != 1) {
+        printf("Usage: ledcns STATE\n");
         return EXIT_FAILURE;
-    ioctl(fd, KDSETLED, state);
-    close(fd);
+    }
+    if ((fd = open("/dev/console", O_WRONLY)) == -1) {
+        perror("open");
+        return EXIT_FAILURE;
+    }
+    if (ioctl(fd, KDSETLED, state) == -1) {
+        perror("ioctl");
+        return EXIT_FAILURE;
+    }
+    if (close(fd) == -1) {
+        perror("close");
+        return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
